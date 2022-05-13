ffa3e045ead43dd720bdc0e1d3428f5af3b7b1d5
shun84 <damien.bres@gmail.com>
2022-05-13 21:11:44 +0200

    - Otpimisation des dépendances

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
-
-PROGRESS_FILE=/tmp/jeedom/RainBird/dependency
+PROGRESS_FILE=/tmp/jeedom/rainbird/dependency
 
 if [ ! -z $1 ]; then
     PROGRESS_FILE=$1
@@ -11,45 +9,38 @@ echo "*************************************"
 echo "*   Launch install of dependencies  *"
 echo "*************************************"
 echo $(date)
-
-BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-function apt_install {
-  sudo apt-get -y install "$@"
-  if [ $? -ne 0 ]; then
-    echo "could not install $1 - abort"
-    rm ${PROGRESS_FILE}
-    exit 1
-  fi
-}
-
-function pip_install {
-  sudo python3 -m pip install "$@"
-  if [ $? -ne 0 ]; then
-    echo "could not install $p - abort"
-    rm ${PROGRESS_FILE}
-    exit 1
-  fi
-}
-
-echo "Version de Python 3 installée :"
-sudo python3 --version
-echo 12 > ${PROGRESS_FILE}
-sudo rm -f /var/lib/dpkg/updates/*
+echo 5 > ${PROGRESS_FILE}
 sudo apt-get clean
-echo 24 > ${PROGRESS_FILE}
+echo 10 > ${PROGRESS_FILE}
+sudo apt-get autoremove
+echo 15 > ${PROGRESS_FILE}
 sudo apt-get update
-echo 36 > ${PROGRESS_FILE}
-echo "Installation des dependances"
-apt_install python3-pip python3-setuptools
+echo 20 > ${PROGRESS_FILE}
+
+echo "*****************************"
+echo "Install modules using apt-get"
+echo "*****************************"
+sudo apt-get install -y python3-pip python3-setuptools
 echo 48 > ${PROGRESS_FILE}
-echo "Installation des dependances Python 3"
-pip_install pycryptodomex
+
+
+echo "*************************************"
+echo "Install the required python libraries"
+echo "*************************************"
+sudo apt-get remove -y python3-crypto
+echo 55 > ${PROGRESS_FILE}
+sudo python3 -m pip uninstall -y pycryptodomex
+echo 58 > ${PROGRESS_FILE}
+sudo python3 -m pip install pycryptodome
 echo 60 > ${PROGRESS_FILE}
-pip_install requests
+sudo python3 -m pip install requests
 echo 72 > ${PROGRESS_FILE}
-pip_install datetime
+sudo python3 -m pip install DateTime
 echo 84 > ${PROGRESS_FILE}
+sudo python3 -m pip install PyYAML
+echo 90 > ${PROGRESS_FILE}
+sudo python3 -m pip install setuptools
+echo 90 > ${PROGRESS_FILE}
 
 echo 100 > ${PROGRESS_FILE}
 echo $(date)
